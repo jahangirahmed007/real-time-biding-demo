0d48c06d77211eab97794ee35a89bf755bbfc244
Store- <jahangirahmedawan007@gmail.com>
2020-01-27 17:46:36 +0500

    server

--- src/App.js
+++ src/App.js
@@ -12,25 +12,16 @@ class App extends React.Component {
     value: 0,
     message: null,
     color: "#0000FF"
-
   }
-  // componentWillMount() { 
-  //   this.socketInit();
-  // };
-
   componentDidMount() {
     this.socketInit();
-
     socket.on("newData", (data) => {
       this.setState({
         value: data.new,
         color: data.color
       })
-
     })
   }
-
-
   socketInit = () => {
     socket = io(ENDPOINT);
     socket.on('userCount', (data) => {
@@ -68,19 +59,14 @@ class App extends React.Component {
     }
     this.Bidding();
   }
-  // });
   Bidding = () => {
     socket.emit("data", { val: this.state.value + 25, color: "#FF0000" });
-
   }
-
-
   render() {
     console.log(this.state);
     return (
       <div className="App">
         <h3>User Count :{this.state.count}</h3>
-
         <CountdownCircleTimer
           key={this.state.value}
           isPlaying
@@ -89,12 +75,8 @@ class App extends React.Component {
             [`${this.state.color}`, 1]
           ]}
         />
-
         <h3> Bidding Amount : {this.state.value}</h3>
-
-
         <button className="btn btn-danger px-4 mt-4" onClick={this.doIncrement} >BID</button>
-
       </div>
     );
   }
